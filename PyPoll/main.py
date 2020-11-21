0a9699067a4a9ccf1d13029b8d0bc0354f509e5c
MSabo <72773063+LongPatrol@users.noreply.github.com>
2020-11-20 21:21:44 -0500

    Candidate list added

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,8 +7,20 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')    
     csv_header = next(csvreader)
 
-    votecount = 0
-
-    for row in csvfile:
+    votecount = 1
+    canlist = []
+    firstline = next(csvreader)
+    candidate = str(firstline[2])
+    canlist.append(candidate)
+  
+ 
+    for row in csvreader:
         votecount = votecount + 1
+        can2 = str(row[2])
+        if can2 not in canlist:
+            canlist.append(can2)
+            
+
+    #print(row[2])
+    print(canlist)   
     print(votecount)
